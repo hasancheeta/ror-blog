99f4686a634d1aa22098608e900b4520b941011c
Hassan Hafeez <hasancheeta@gmail.com>
2022-10-25 19:55:48 +0500

    aded constraint to restrict id param to be numeric using reg expression. We can get specific product using /products/1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'products#index'
   get 'products', to: 'products#index', as: 'products'
-  get 'products/:id', to: 'products#show', as: 'product'
+  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
