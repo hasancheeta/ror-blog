bfe60622be892967d50970b13e30a24b06c7efe1
Hassan Hafeez <hasancheeta@gmail.com>
2022-10-27 15:16:53 +0500

    Implemmented before_action and partials etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'products#index'
-  get 'products', to: 'products#index', as: 'products'
-  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
+  resources :products
+  #get 'products', to: 'products#index', as: 'products'
+  #get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
